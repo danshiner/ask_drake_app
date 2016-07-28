97484d0f23531d10d32755e6775db65479729225
danshiner <danshiner@gmail.com>
2016-07-28 10:01:52 -0400

    Added condition so that image only sends every 66 questions

--- app/services/sender.rb
+++ app/services/sender.rb
@@ -14,14 +14,25 @@ class Sender
         account_sid = 'AC6533ddc2b095658337840937b068c062'
         auth_token = '6a02e4987794c0ac52e40b35e1bf699a'
         client = Twilio::REST::Client.new(account_sid, auth_token)
-        client.messages.create(
-          to: question.user.phone_number,
-          from: "+1647722DRIZ",
-          # Careful with commas!
-          #body: "#{@draketip.lyric.lyric} \n\n\u260E hotline ping \n#{question.user.credits} pings remaining",
-          body: "#{question.user.credits} pings remaining",
-          media_url: @draketip.img_url
-        )
+        if (@draketip.id % 66 == 0)
+          client.messages.create(
+            to: question.user.phone_number,
+            from: "+1647722DRIZ",
+            # Careful with commas!
+            # body: "#{@draketip.lyric.lyric} \n\nhotline ping \u260E \nping no #{100000+@draketip.id} \n#{question.user.credits} pings remaining"
+            body: "#{question.user.credits} pings remaining",
+            media_url: @draketip.img_url
+          )
+        else
+          client.messages.create(
+            to: question.user.phone_number,
+            from: "+1647722DRIZ",
+            # Careful with commas!
+            body: "#{@draketip.lyric.lyric} \n\nhotline ping \u260E \nping no #{100000+@draketip.id} \n#{question.user.credits} pings remaining"
+            # body: "#{question.user.credits} pings remaining",
+            # media_url: @draketip.img_url
+          )
+        end
       when :twitter
         # message = replace_variables("#USER#", tweet)
         # client.update_with_media message, File.open("./public/draketips/draketip_#{100000+@draketip.id}.png"), in_reply_to_status_id:tweet.id
